a7fb0302fe4ba0a582f55f55331f0126099bccde
ollatomiwa <ajaomoshood@gmail.com>
2024-03-06 21:39:43 +0100

    Dummy part done, next is installing our tailwindcss, daisyUI

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-const express = require('express');
-const axios = require ('axios');
-const http = require ('http');
-const app = express ();
-const PORT = process.env.PORT || 3000;
+// const express = require("express");
+// const axios = require("axios");
+// const http = require("http");
+// const app = express();
+// const PORT = process.env.PORT || 3000;
 
-app.set ('view engine', 'ejs');
+// app.set("view engine", "ejs");
 
-app.get ('/', async (req, res) => {
-    try {
-        const response = await  axios.get('api/');
-        const liveScores = response.data();
-        res.render('index', {liveScores});
-    
-    } catch (error) {
-        console.error('Error fetching live scores:', error);
-        res.status(500).send('Error fetching live scores')
-        
-    };
-});
+// app.get("/", async (req, res) => {
+//   try {
+//     const response = await axios.get(
+//       "https://apiv3.apifootball.com/?action=get_countries&APIkey=https://apiv3.apifootball.com/?action=get_countries&APIkey=abdacef9f0e555dd193628cd18f5da0c6d03a3c14141d49089f2f40d322bbbed"
+//     );
+//     const liveScores = response.data() {
+//         return {
+            
+//         }
+//     },
+//     res.render("index", { liveScores });
+//   } catch (error) {
+//     console.error("Error fetching live scores:", error);
+//     res.status(500).send("Error fetching live scores");
+//   }
+// });
 
-app.listen( PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
