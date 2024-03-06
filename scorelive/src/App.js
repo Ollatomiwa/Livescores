ce3dae4a3ddf136852fe42931c6f73794581f9a2
ollatomiwa <ajaomoshood@gmail.com>
2024-03-06 22:23:48 +0100

    daisyUl used to setup NavBar, next is to use RapidApi

---
+++ scorelive/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from "./components/navbar";
+
+function App() {
+  return <div className="">
+    <NavBar />
+
+
+
+    </div>;
+
+}
+
+export default App;
---
+++ scorelive/src/components/navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function navbar() {
+  return (
+    <div className="navbar bg-base-100" data-theme="dark">
+      <a className="btn btn-ghost text-xl">LiveScores</a>
+    </div>
+  );
+}
